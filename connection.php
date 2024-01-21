ed8fce1f3643d1dfde2820a889cac90f5ccee4ed
UTTAM KUMAR SAHOO <83549465+uttam-kumar-sahoo@users.noreply.github.com>
2024-01-21 21:22:03 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+
+
+<?php
+//change mysqli_connect(host_name,username, password); 
+$connection = mysqli_connect("sql112.infinityfree.com", "if0_35803345", "CV1sT1a4Qr");
+$db = mysqli_select_db($connection, 'if0_35803345_demo');
+?>
